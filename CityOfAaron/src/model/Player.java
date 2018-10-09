04d0d8d04ef8c65b6b9dd6f3bae748e669f0e33a
chelsea <chelseawaite@192.168.0.11>
2018-10-08 17:24:15 -0700

    updated classes

---
+++ CityOfAaron/src/model/Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author chelseawaite
+ */
+public class Player {
+   
+   @Override
+   public String toString(){
+      return "Player class coming soon";
+   }
+   
+}
