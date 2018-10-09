7a785d2e06a5711dffdec439f4aea4e2c248cb1e
chelsea <chelseawaite@192.168.0.11>
2018-10-08 19:49:51 -0700

    Map, Point, and Location classes

--- CityOfAaron/src/model/Map.java
+++ CityOfAaron/src/model/Map.java
@@ -4,20 +4,39 @@
  * and open the template in the editor.
  */
 package model;
+import java.io.Serializable;
 
 /**
  *
  * @author chelseawaite
  */
-public class Map {
+public class Map implements Serializable{
+   
+   private Location[] locations;
+   private Point currentLocation;
    
    public Map(){
       // empty constructor for Javabeans
    }
    
+   public Location[] getLocation(){
+      return locations;
+}
+   
+   public Point getCurrentLocation(){
+      return currentLocation;
+   }
+   
+   public void setCurrentLocation(Point point){
+      this.currentLocation = currentLocation;
+   }
+   
    @Override
    public String toString(){
-      return "Map class coming soon";
+      return "Map{" 
+              + "locations" + locations
+              + "currentLocation" + currentLocation
+              + "}";
    }
    
 }
