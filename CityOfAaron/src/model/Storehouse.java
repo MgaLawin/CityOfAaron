8990bed31ff1e262f51146cf7c85ebc19e29ccd9
MgaLawin <35164043+MgaLawin@users.noreply.github.com>
2018-10-08 21:29:56 -0700

    Creating models

--- CityOfAaron/src/model/Storehouse.java
+++ CityOfAaron/src/model/Storehouse.java
@@ -5,18 +5,49 @@
  */
 package model;
 
+import java.io.Serializable;
+
 /**
  *
- * @author chelseawaite
+ * @author Jeremy
  */
-public class Storehouse {
-   
-   public Storehouse(){
-      // empty constructor for Javabeans
-   }
-   
-   @Override
-   public String toString(){
-      return "Storehouse class coming soon";
-   }
+public class Storehouse implements Serializable {
+
+    private Author[] authors;
+    private Animal[] animals;
+    private InventoryItem[] tools;
+    private Provision[] provisions;
+
+    public Storehouse() {
+        // empty constructor for Javabeans
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author[] authors) {
+        this.authors = authors;
+    }
+
+    public Animal[] getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(
+
+        (Animal[] animals) {
+        this.animals = animals;
+    }
+
+    @Override
+    public String toString() {
+        return "Storehouse{"
+                + "authors" + author
+                + "animals" + animal
+                + "mapSymbols" + mapSymbol
+                + "gameTips" + gameTips
+                + "}";
+    }
+
 }
