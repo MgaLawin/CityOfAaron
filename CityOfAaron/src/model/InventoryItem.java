af6b7205c4ba369daec34ed562f711034c396ac3
Deon Miller <didimillermom@gmail.com>
2018-10-09 13:29:55 -0700

    I have added the InventoryItem, Condition, Itemtype. I am still working on Inventory Item but will add shortly

---
+++ CityOfAaron/src/model/InventoryItem.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+import java.io.Serializable;
+
+/**
+ *
+ * @author DIDIM
+ */
+public class InventoryItem  implements Serializable{
+    private String name;
+    private ItemType(enum) ItemType;
+    private int quanity
+    private Condition(enum) condition;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+   public ItemType getName() 
+}
