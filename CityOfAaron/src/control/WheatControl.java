ffb8a7375bf9cb49a60b86d6b67f0f03c0ba44b3
chelsea <chelseawaite@10.120.3.88>
2018-10-25 13:23:33 -0700

    Finished the People Control and it runs without errors

--- CityOfAaron/src/control/WheatControl.java
+++ CityOfAaron/src/control/WheatControl.java
@@ -32,7 +32,7 @@ public class WheatControl {
         
         //return wheatInStorage * percentLost --will need to be turned back into an int
         
-    //return -100; //DEON you can change this, I just added this return so that it won't give an error when I tried to run my code -Chelsea
+    return -100; //DEON you can change this, I just added this return so that it won't give an error when I tried to run my code -Chelsea
     }
     
     public static int calculateHarvest(int tithingPercent, int acresPlanted){
