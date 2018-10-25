f93cf79541054406159d5f432d0f8cebc5e182c1
Deon Miller <didimillermom@gmail.com>
2018-10-25 14:17:46 -0600

    Added the pseudo code for the Wheat control based on correct post.

--- CityOfAaron/src/control/GameControl.java
+++ CityOfAaron/src/control/GameControl.java
@@ -7,51 +7,47 @@ package control;
 
 import java.util.Random;
 
-
 /**
  *
  * @author chelseawaite
  */
 public class GameControl {
-   
-private static Random randomGenerator  = new Random();
+
+    private static Random randomGenerator = new Random();
 
 // create a a protected random number object for testing
-protected static void setRandomGenerator(Random random){
-    randomGenerator = random;
-}
-
-
-public static int getRandomNumber(int lowNumber, int highNumber){
-
-    //if low < 0 or high< 0 then return -1
-    if (lowNumber < 0 || highNumber <0){
-        return -1;
+    protected static void setRandomGenerator(Random random) {
+        randomGenerator = random;
     }
-    
-    //if high <= low return -2
-     if (highNumber <= lowNumber){
-         return -2;
-     }
-    
-    //if high is the max value for the integers return -3
-    if (highNumber == Integer.MAX_VALUE){
-        return -3;
-    }
-    // calculate the size of the range and add one so Random() includes highNumber
-    int range = (highNumber - lowNumber) + 1;
+
+    public static int getRandomNumber(int lowNumber, int highNumber) {
+
+        //if low < 0 or high< 0 then return -1
+        if (lowNumber < 0 || highNumber < 0) {
+            return -1;
+        }
+
+        //if high <= low return -2
+        if (highNumber <= lowNumber) {
+            return -2;
+        }
+
+        //if high is the max value for the integers return -3
+        if (highNumber == Integer.MAX_VALUE) {
+            return -3;
+        }
+        // calculate the size of the range and add one so Random() includes highNumber
+        int range = (highNumber - lowNumber) + 1;
 
 //return low and random(range size)
-   return lowNumber + randomGenerator.nextInt(range);
-  
-}
+        return lowNumber + randomGenerator.nextInt(range);
 
+    }
 
-public static String createNewGame(String playerName){
+    public static String createNewGame(String playerName) {
         //Returns a new Game object with everything initialized and ready for the first year of play.
-        
-    return "Game"; 
-}
 
+        return "Game";
+    }
 
 }
--- CityOfAaron/test/control/GameControlTest.java
+++ CityOfAaron/test/control/GameControlTest.java
@@ -3,13 +3,12 @@ package control;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 /**
  *
  * @author DIDIM
  */
 public class GameControlTest {
-    
+
     /**
      * Test of getRandomNumber method, of class GameControl.
      */
@@ -19,54 +18,51 @@ public class GameControlTest {
         int result = GameControl.getRandomNumber(-1, 10);
         assertEquals(-1, result);
 
-       
         result = GameControl.getRandomNumber(10, -1);
         assertEquals(-1, result);
-       
+
     }
-    
+
 //Test high for low and same values
-  @Test
+    @Test
     public void testGetRandomNumber2() {
         int result = GameControl.getRandomNumber(100, 10);
         assertEquals(-2, result);
-      
+
         result = GameControl.getRandomNumber(5, 5);
         assertEquals(-2, result);
-        
+
     }
 //    test max limit
+
     @Test
-    public void testGetRandomNumber3(){
+    public void testGetRandomNumber3() {
         int result = GameControl.getRandomNumber(0, Integer.MAX_VALUE);
         assertEquals(-3, result);
-        
+
     }
+
     // test all valid using multi-dimensional array and for loop
     @Test
-    public void testGetRandomNumberValid(){
-        int[] [] testRanges= new int[] []{
-            {1,10},
-            {15,100},
-            {5,6},
-            {0, Integer.MAX_VALUE -1}
+    public void testGetRandomNumberValid() {
+        int[][] testRanges = new int[][]{
+            {1, 10},
+            {15, 100},
+            {5, 6},
+            {0, Integer.MAX_VALUE - 1}
         };
-        
-            
-                for (int i=0; i < testRanges.length; i++){
-                    int low = testRanges [i][0];
-                    int high= testRanges [i][1];
-                    
-                    int random= GameControl.getRandomNumber (low, high);
-                    System.out.printf("%d <= %d <= %d\n" , low, random, high);
-                    
-                    assertTrue(
-                    String.format("%d should be between %d and %d", random, low, high), 
-                            random >= low && random <= high);
-                    
-                    
-                           
-                    
+
+        for (int i = 0; i < testRanges.length; i++) {
+            int low = testRanges[i][0];
+            int high = testRanges[i][1];
+
+            int random = GameControl.getRandomNumber(low, high);
+            System.out.printf("%d <= %d <= %d\n", low, random, high);
+
+            assertTrue(
+                    String.format("%d should be between %d and %d", random, low, high),
+                    random >= low && random <= high);
+
         }
     }
 }
