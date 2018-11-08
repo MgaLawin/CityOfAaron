74db27493afc01a72ba90a336a47a52c661d7aed
MgaLawin <35164043+MgaLawin@users.noreply.github.com>
2018-11-07 18:19:57 -0800

    11-07-2018 Adjusted LiveTheYear to be a control instead of a view

---
+++ CityOfAaron/src/control/LiveTheYear.java
@@ -0,0 +1,56 @@
+package control;
+
+/**
+ *
+ * @author Jeremy
+ */
+public class LiveTheYear {
+
+    /**
+     * Process the current year's results and update the Game object
+     *
+     * @param game The current Game object (pass by reference)
+     * @param tithingPercent The percentage of tithing selected for the year
+     * @param bushelsForFood The number of bushels of wheat allocated as food
+     * for the year
+     * @param acresPlanted The number of acres to be used for planting
+     *
+     * @return The year's Annual Report data
+     */
+    public static AnnualReport liveTheYear(
+            Game game, int tithingPercent,
+            int bushelsForFood, int acresPlanted) {
+        if (game == null || tithingPercent < 0 || tithingPercent > 100
+                || bushelsForFood < 0 || acresPlanted < 0) {
+            return null;
+        }
+
+        AnnualReport report = new AnnualReport();
+        report.setLandPrice(LandControl.getCurrentLandPrice());
+
+        int totalWheat = game.getWheatInStorage();
+
+        int harvested = WheatControl.calculateHarvest(tithingPercent, acresPlanted);
+        int tithingPercent = (int) (double) ((tithingPercentagePaid / 100.0) * harvested);
+        int lostToRats = WheatControl.calculateLossToRats(tithingPercent, totalWheat);
+
+        int peopleStarved = PeopleControl.calculateMortality(bushelsForFood, game.getCurrentPopulation());
+        int peopleMovedIn = PeopleControl.calculateNewMoveIns(game.getCurrentPopulation());
+
+        totalWheat = totalWheat + harvested - tithingAmount - lostToRats;
+        game.setWheatInStorage(totalWheat);
+        report.setCurrentPopulation(game.getCurrentPopulation() - peopleStarved + peopleMovedIn);
+
+        report.setBushelsHarvested(harvested);
+        report.setTithingAmount(tithingAmount);
+        report.setLostToRats(lostToRats);
+        report.setPeopleStarved(peopleStarved);
+        report.setPeopleMovedIn(peopleMovedIn);
+
+        report.setEndingWheatInStorage(game.getWheatInStorage());
+        report.setEndingPopulation(game.getCurrentPopulation());
+        report.setEndingAcresOwned(game.getAcresOwned());
+
+        return report;
+    }
+}
