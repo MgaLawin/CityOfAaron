162019978e1d3987e2da312577b7cf17d39f6c67
MgaLawin <35164043+MgaLawin@users.noreply.github.com>
2018-11-14 23:52:08 -0800

    11-14-2018 Adjusted the Banner

--- CityOfAaron/src/view/StartProgramView.java
+++ CityOfAaron/src/view/StartProgramView.java
@@ -22,13 +22,13 @@ public class StartProgramView extends ViewBase {
     protected String getMessage() {
 
         return "\n\n****************************************************************************************\n"
-                + " Welcome to the city of Aaron. You have been chosen be the chief ruler in the city.\n"
+                + " Welcome to the city of Aaron. You have been chosen to be the chief ruler in the city.\n"
                 + " Your responsibilities are to buy and sell land, determine how much wheat to plant each year\n "
                 + "and how much to set aside to feed the people. You are also responsible to pay the annual\n "
                 + "tithing on the wheat that is harvested. If you fail to provide enough wheat for the people,\n"
-                + " people will starve, some people will die, and your workforce will be diminished.\n  "
-                + "If too many people die you will be kicked out of office and  the game will end. \n"
-                + "Plan carefully. Also beware, there is always a danger of rats eating your wheat . . .\n"
+                + " people will starve, people will die, and your workforce will be diminished.\n  "
+                + "If too many people die, you will be kicked out of office and  the game will end. \n"
+                + "Plan carefully. Also be aware that there is always a danger of rats eating your wheat . . .\n"
                 + "**************************************************************************************************\n";
 
     }
